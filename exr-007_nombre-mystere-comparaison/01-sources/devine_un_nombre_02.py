a11f3baa00c9cde6ab7beabaa6fc50726982589e
Anass YAKINE <anassyakine06@gmail.com>
2020-04-28 20:12:48 +0200

    commit chp-002

--- exr-007_nombre-mystere-comparaison/01-sources/devine_un_nombre_02.py
+++ exr-007_nombre-mystere-comparaison/01-sources/devine_un_nombre_02.py
@@ -3,3 +3,10 @@ nombre_utilisateur = input("Quel est le nombre mystère ? ")
 
 # Afficher à l'aide d'une structure conditionnelle si le nombre entré par l'utilisateur est plus grand,
 # plus petit ou égal au nombre mystère.
+
+if nombre_mystere > int(nombre_utilisateur):
+    print("le nombre est plus petit")
+elif nombre_mystere < int(nombre_utilisateur):
+    print("le nombre est plus grand")
+elif nombre_mystere == int(nombre_utilisateur):
+    print("le nombre est exacte")
