b400abd172bcc4cff0d29e22c1b58785c9817709
Anass YAKINE <anassyakine06@gmail.com>
2020-05-01 19:26:50 +0200

    commit exo 15

--- exr-015_nombre-mystere-essais/01-sources/devine_un_nombre_05.py
+++ exr-015_nombre-mystere-essais/01-sources/devine_un_nombre_05.py
@@ -1,17 +1,33 @@
 import random
 
 nombre_mystere = random.randint(0, 10)
-nombre = input("Quel est le nombre mystère ? ")
-if not nombre.isdigit():
-    print("SVP, entrez un nombre valide.")
-    exit()
 
-nombre = int(nombre)
+i = 0
+is_win = False
+while(i < 5):
+    nombre = input("Quel est le nombre mystère ? ")
+    if not nombre.isdigit():
+        print("SVP, entrez un nombre valide.")
+        i += 1
+        continue
 
-if nombre > nombre_mystere:
-    print(f"Le nombre mystère est plus petit que {nombre}")
-elif nombre < nombre_mystere:
-    print(f"Le nombre mystère est plus grand que {nombre}")
-else:
-    print("Bravo, vous avez trouvé le nombre mystère !")
+    nombre = int(nombre)
+    if nombre > nombre_mystere:
+        print(f"Le nombre mystère est plus petit que {nombre}")
+        print(f"Nombre essaie:{i+1}")
+        i += 1
+        continue
+    elif nombre < nombre_mystere:
+        print(f"Le nombre mystère est plus grand que {nombre}")
+        print(f"Nombre essaie:{i+1}")
+        i += 1
+        continue
+    else:
+        print("Bravo, vous avez trouvé le nombre mystère !")
+        is_win=True
+        break
+
+if(is_win == False):
+    print(f"vous avez perdu le nombre mystère {nombre_mystere}")
+    
 
